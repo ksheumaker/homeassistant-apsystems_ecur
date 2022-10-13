610bfdc77648db1f79506d2282b15ea63ae88e58
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2022-10-13 17:49:15 +0200

    Update const.py

--- custom_components/apsystems_ecur/const.py
+++ custom_components/apsystems_ecur/const.py
@@ -4,6 +4,7 @@ FREQ_ICON = "mdi:sine-wave"
 SIGNAL_ICON = "mdi:signal"
 RELOAD_ICON = "mdi:reload"
 CACHE_ICON = "mdi:cached"
+RESTART_ICON = "mdi:restart"
 
 CONF_REOPEN_SOCKET = "reopen_socket"
 CONF_QUERY_METHOD = "query_method"

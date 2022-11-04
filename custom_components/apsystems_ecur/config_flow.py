abff66268cebcd65fcde3c33b7351dc5b382bd51
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2022-11-04 12:31:13 +0100

    Update config_flow.py
    Cleanup code:
    - Removed obsolete code for Invalid/unknown inverters
    - Corrected configuration options placement of parameters

--- custom_components/apsystems_ecur/config_flow.py
+++ custom_components/apsystems_ecur/config_flow.py
@@ -3,7 +3,7 @@ import voluptuous as vol
 import traceback
 from datetime import timedelta
 from homeassistant.core import callback
-from .APSystemsSocket import APSystemsSocket, APSystemsInvalidData, APSystemsInvalidInverter
+from .APSystemsSocket import APSystemsSocket, APSystemsInvalidData
 from homeassistant import config_entries, exceptions
 from homeassistant.const import CONF_HOST, CONF_SCAN_INTERVAL
 import homeassistant.helpers.config_validation as cv
@@ -11,26 +11,14 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-from .const import DOMAIN, CONF_REOPEN_SOCKET, CONF_QUERY_METHOD
-
-CONFIG_SCHEMA = vol.Schema({
-        vol.Required(CONF_HOST): str,
-        #vol.Required(
-        #    CONF_QUERY_METHOD,
-        #    default="socket",
-        #): vol.In(["socket", "http"]),
-        vol.Optional(
-            CONF_SCAN_INTERVAL, 
-            default=300
-        ) : int,
-    })
-    
+from .const import DOMAIN
 
 @config_entries.HANDLERS.register(DOMAIN)
 class APSsystemsFlowHandler(config_entries.ConfigFlow):
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+    #connection classes are deprecated since 2021.6.0
+    #CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     def __init__(self):
         _LOGGER.debug("Starting config flow class...")
@@ -53,16 +41,21 @@ class APSsystemsFlowHandler(config_entries.ConfigFlow):
             except APSystemsInvalidData as err:
                 _LOGGER.exception(f"APSystemsInvalidData exception: {err}")
                 errors["host"] = "cannot_connect"
-            except APSystemsInvalidInverter as err:
-                _LOGGER.exception(f"APSystemsInvalidInverter exception: {err}")
-                errors["host"] = "unknown_inverter"
             except Exception as err:
-                _LOGGER.exception(f"Unhandled exception: {err}")
+                _LOGGER.exception(f"Unknown error occurred during setup: {err}")
                 errors["host"] = "unknown"
 
         _LOGGER.debug("Returning to show form")
         return self.async_show_form(
-            step_id="user", data_schema=CONFIG_SCHEMA, errors=errors
+            step_id="user",
+            errors=errors,
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_HOST): str,
+                    vol.Optional(CONF_SCAN_INTERVAL, default=300): int,
+                    
+                }
+            )
         )
 
     async def async_step_import(self, user_input):
@@ -75,32 +68,39 @@ class APSsystemsFlowHandler(config_entries.ConfigFlow):
         return APSsystemsOptionsFlowHandler(config_entry)
 
 class APSsystemsOptionsFlowHandler(config_entries.OptionsFlow):
-    
-    def __init__(self, config_entry):
+    def __init__(self, config_entry: config_entries.ConfigEntry):
         self.config_entry = config_entry
 
-    async def async_step_init(self, _user_input=None):
+    async def async_step_init(self, user_input=None):
         return await self.async_step_user()
 
-    async def async_step_user(self, user_input=None):
-        schema = self.config_entry.options
+    async def async_step_init(self, user_input=None):
+        errors = {}
         if user_input is not None:
             try:
                 ap_ecu = APSystemsSocket(user_input["host"])
                 test_query = await self.hass.async_add_executor_job(ap_ecu.query_ecu)
                 ecu_id = test_query.get("ecu_id", None)
                 if ecu_id != None:
-                    return self.async_create_entry(title=f"ECU: {ecu_id}", data=user_input)
+                    self.hass.config_entries.async_update_entry(
+                    self.config_entry, data=user_input, options=self.config_entry.options
+                    )
+                    return self.async_create_entry(title=f"ECU: {ecu_id}", data={})
                 else:
                     errors["host"] = "no_ecuid"
-
             except APSystemsInvalidData as err:
-                _LOGGER.exception(f"APSystemsInvalidData exception: {err}")
                 errors["host"] = "cannot_connect"
-            except APSystemsInvalidInverter as err:
-                _LOGGER.exception(f"APSystemsInvalidInverter exception: {err}")
-                errors["host"] = "unknown_inverter"
             except Exception as err:
-                _LOGGER.exception(f"Unhandled exception: {err}")
+                _LOGGER.warning(f"Unknown error occurred during setup: {err}")
                 errors["host"] = "unknown"
-        return self.async_show_form(step_id="user", data_schema=CONFIG_SCHEMA)
+
+        return self.async_show_form(
+            step_id="init",
+            errors=errors,
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_HOST, default=self.config_entry.data.get(CONF_HOST)): str,
+                    vol.Optional(CONF_SCAN_INTERVAL, default=self.config_entry.data.get(CONF_SCAN_INTERVAL)): int,
+                }
+            )
+        )

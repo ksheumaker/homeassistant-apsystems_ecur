6dbd7adfb4a1b569f8cc627952053cb08ebea013
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2022-11-26 19:12:36 +0100

    Update config_flow.py

--- custom_components/apsystems_ecur/config_flow.py
+++ custom_components/apsystems_ecur/config_flow.py
@@ -87,25 +87,9 @@ class APSsystemsOptionsFlowHandler(config_entries.OptionsFlow):
                     self.hass.config_entries.async_update_entry(
                     self.config_entry, data=user_input, options=self.config_entry.options
                     )
-                    
-                    
-                    
-                    
-                                        
-                    update_interval = timedelta(seconds=self.config_entry.data.get(CONF_SCAN_INTERVAL))
-                         
-                    coordinator = DataUpdateCoordinator(
-                        self.hass,
-                        _LOGGER,
-                        name=DOMAIN,
-                        # Name of the data. For logging purposes.
-                        update_interval=timedelta(seconds=self.config_entry.data.get(CONF_SCAN_INTERVAL))
-                    )
-                    await coordinator.async_request_refresh()     
-                    
-                    
-                    
-                    
+                    coordinator = self.hass.data[DOMAIN].get("coordinator")
+                    coordinator.update_interval = timedelta(seconds=self.config_entry.data.get(CONF_SCAN_INTERVAL))
+                    coordinator.async_refresh()    
                     return self.async_create_entry(title=f"ECU: {ecu_id}", data={})
                 else:
                     errors["host"] = "no_ecuid"

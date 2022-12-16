5400e8e69d36aaa3e13141dcdee5d29854a2734a
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2022-12-16 10:54:14 +0100

    Update config_flow.py

--- custom_components/apsystems_ecur/config_flow.py
+++ custom_components/apsystems_ecur/config_flow.py
@@ -98,7 +98,7 @@ class APSsystemsOptionsFlowHandler(config_entries.OptionsFlow):
         return self.async_show_form(
             step_id="init",
             errors=errors,
-                data_schema=vol.Schema(
+            data_schema=vol.Schema(
                 {
                     vol.Required(CONF_HOST, default=self.config_entry.data.get(CONF_HOST)): str,
                     vol.Optional(CONF_SCAN_INTERVAL, default=300, 
@@ -109,6 +109,6 @@ class APSsystemsOptionsFlowHandler(config_entries.OptionsFlow):
                         description={"suggested_value": self.config_entry.data.get(CONF_SSID)}): str,
                     vol.Optional(CONF_WPA_PSK, default="myWiFipassword", 
                         description={"suggested_value": self.config_entry.data.get(CONF_WPA_PSK)}): str,
-                })
+                }
             )
         )

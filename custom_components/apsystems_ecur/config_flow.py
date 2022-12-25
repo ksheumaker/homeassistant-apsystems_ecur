3521fdcffb719cac154dc4362dd9d4f059629b5f
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2022-12-25 10:09:27 +0100

    Update config_flow.py

--- custom_components/apsystems_ecur/config_flow.py
+++ custom_components/apsystems_ecur/config_flow.py
@@ -71,6 +71,7 @@ class APSsystemsFlowHandler(config_entries.ConfigFlow):
 
 class APSsystemsOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry: config_entries.ConfigEntry):
+        _LOGGER.debug("Starting options flow class...")
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
